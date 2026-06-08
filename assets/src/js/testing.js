198a89e7a5033382c05ca65166aeef02fc482d8a
Steve Ferrar <steveferrar@gmail.com>
2026-06-08 11:35:37 +0100

    v1.0.1 styles

--- assets/src/js/testing.js
+++ assets/src/js/testing.js
@@ -1,8 +1,8 @@
 /** Testing Scripts */
 jQuery(document).ready(function($){
 
-    $('#PhaseOne').addClass('jsPhaseActive');
-	$('.phaseContainer').addClass('jsPhaseActive').attr('data-phase', 'PhaseOne');
+    // $('#PhaseOne').addClass('jsPhaseActive');
+	// $('.phaseContainer').addClass('jsPhaseActive').attr('data-phase', 'PhaseOne');
     // $('#PhaseTwo').addClass('jsPhaseActive');
 	// $('.phaseContainer').addClass('jsPhaseActive').attr('data-phase', 'PhaseTwo');
     // $('#PhaseThree').addClass('jsPhaseActive');
